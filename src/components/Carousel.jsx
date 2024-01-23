cc7a531f2e424acd313f7245cdcbdea4662e118a
Ichivin <kevinblondiau@gmail.com>
2024-01-23 16:50:03 +0100

    finish lodging page

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import arrowRight from "../images/arrow-right.svg";
+import arrowLeft from "../images/arrow-left.svg";
+
+function Carousel({ pictures }) {
+    const [current, setCurrent] = useState(0);
+    return (
+        <div className="carousel">
+            <img className="carousel__img" alt="" src={pictures[current]} />
+            <img
+                onClick={() => {
+                    setCurrent(current === 0 ? pictures.length - 1 : current - 1);
+                }}
+                src={arrowLeft}
+                alt=""
+                className="carousel__arrow-left"
+            />
+            <img
+                onClick={() => {
+                    setCurrent(current === pictures.length - 1 ? 0 : current + 1);
+                }}
+                src={arrowRight}
+                alt=""
+                className="carousel__arrow-right"
+            />
+            <p className="carousel__numbering">
+                {current + 1}/{pictures.length}
+            </p>
+        </div>
+    );
+}
+
+export default Carousel;
--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -3,36 +3,61 @@ import { useParams } from "react-router-dom";
 import lodgings from "../data/logement.json";
 import Accordion from "../components/Accordion";
 import Rating from "../components/Rating";
+import Carousel from "../components/Carousel";
 
 function Lodging() {
     const { id } = useParams();
     const [lodging, setLodging] = useState({});
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         setLodging(lodgings.find((item) => item.id === id));
+        setLoading(false);
     }, [id]);
-    return (
-        <div className="lodging">
-            <h1 className="lodging__title">{lodging.title}</h1>
-            <h2 className="lodging__location">{lodging.location}</h2>
-            <ul className="lodging__tags">{lodging.tags && lodging.tags.map((tag, i) => <li key={i}>{tag} </li>)}</ul>
-            {lodging.host && (
-                <div className="lodging__profil">
-                    <p className="lodging__host-name">{lodging.host.name}</p>
-                    <img className="lodging__host-picture" src={lodging.host.picture} alt={lodging.host.name} />
+    return loading ? (
+        <div>loading</div>
+    ) : (
+        <main className="lodging">
+            <Carousel pictures={lodging.pictures} />
+            <div className="lodging__wrapper">
+                <div className="lodging__left">
+                    <h1 className="lodging__title">{lodging.title}</h1>
+                    <h2 className="lodging__location">{lodging.location}</h2>
+                    <ul className="lodging__tags">
+                        {lodging.tags &&
+                            lodging.tags.map((tag, i) => (
+                                <li className="lodging__tags-background" key={i}>
+                                    {tag}{" "}
+                                </li>
+                            ))}
+                    </ul>
                 </div>
-            )}
-            <div className="loding__rating">
-                <Rating rating={lodging.rating} />
+                <div className="lodging__right">
+                    {lodging.host && (
+                        <div className="lodging__profil">
+                            <p className="lodging__host-name">{lodging.host.name}</p>
+                            <img className="lodging__host-picture" src={lodging.host.picture} alt={lodging.host.name} />
+                        </div>
+                    )}
+                    <div className="lodging__rating">
+                        <Rating rating={lodging.rating} />
+                    </div>
+                </div>
             </div>
             <div className="lodging__accordions">
                 <Accordion title={"Description"}>
                     <p className="lodging__description">{lodging.description}</p>
                 </Accordion>
                 <Accordion title={"Équipements"}>
-                    <p className="lodging__equipments">{lodging.equipments}</p>
+                    <ul className="lodging__equipments">
+                        {lodging.equipments.map((equipment) => (
+                            <li className="lodging__equipment" key={equipment}>
+                                {equipment}
+                            </li>
+                        ))}
+                    </ul>
                 </Accordion>
             </div>
-        </div>
+        </main>
     );
 }
 
